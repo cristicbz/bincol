9bfe5fe590225ac6aef91218e9bc1c8334c64253
Cristi Cobzarenco <cristi.cobzarenco@gmail.com>
2026-01-06 11:33:08 +0000

    wip

--- src/tests/evolution.rs
+++ src/tests/evolution.rs
@@ -1,37 +1,69 @@
 use super::helpers::{check_evolution_fails, check_evolution_ok};
 use serde::{
-    Deserialize, Serialize,
     de::{DeserializeOwned, IgnoredAny},
+    Deserialize, Serialize,
 };
-use std::fmt::Debug;
+use serde_bytes::ByteBuf;
+use std::{fmt::Debug, marker::PhantomData};
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct UnitStruct;
+#[derive(Debug)]
+struct UnitStruct<T = String> {
+    phantom: PhantomData<T>,
+}
 
-impl From<String> for UnitStruct {
-    fn from(_field: String) -> Self {
-        Self
+impl<T> From<T> for UnitStruct<T> {
+    fn from(_field: T) -> Self {
+        Self {
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<T> Serialize for UnitStruct<T> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_unit_struct("UnitStruct")
+    }
+}
+
+impl<'de, T> Deserialize<'de> for UnitStruct<T> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct Visitor<T>(PhantomData<T>);
+        impl<'de, T> serde::de::Visitor<'de> for Visitor<T> {
+            type Value = UnitStruct<T>;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(formatter, "UnitStruct")
+            }
+
+            fn visit_unit<E>(self) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                Ok(UnitStruct {
+                    phantom: PhantomData,
+                })
+            }
+        }
+        deserializer.deserialize_unit_struct("UnitStruct", Visitor(PhantomData))
     }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct NewType<T = String>(T);
 
-impl<T> From<String> for NewType<T>
-where
-    T: From<String>,
-{
-    fn from(field: String) -> Self {
-        Self(field.into())
+impl<T> NewType<T> {
+    fn new(field: T) -> Self {
+        Self(field)
     }
-}
 
-impl<T> AsRef<str> for NewType<T>
-where
-    T: AsRef<str>,
-{
-    fn as_ref(&self) -> &str {
-        self.0.as_ref()
+    fn as_inner(&self) -> &T {
+        &self.as_inner()
     }
 }
 
@@ -102,7 +134,7 @@ where
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Debug)]
 #[serde(transparent)]
 pub struct UnitVariant<T = String>(Enum<T>);
 
@@ -115,7 +147,7 @@ where
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Debug)]
 #[serde(transparent)]
 pub struct NewTypeVariant<T = String>(Enum<T>);
 
@@ -137,7 +169,7 @@ where
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Debug)]
 #[serde(transparent)]
 pub struct TupleVariant<T = String, U = ()>(Enum<T, U>);
 
@@ -160,7 +192,7 @@ where
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Debug)]
 #[serde(transparent)]
 pub struct StructVariant<T = String>(Enum<T>);
 
@@ -186,9 +218,9 @@ where
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(transparent)]
-struct OptionSome<T = String>(Option<T>);
+struct Optional<T = String>(Option<T>);
 
-impl<T> From<String> for OptionSome<T>
+impl<T> From<String> for Optional<T>
 where
     T: From<String>,
 {
@@ -197,7 +229,7 @@ where
     }
 }
 
-impl<T> AsRef<str> for OptionSome<T>
+impl<T> AsRef<str> for Optional<T>
 where
     T: AsRef<str>,
 {
@@ -206,7 +238,7 @@ where
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Debug)]
 #[serde(transparent)]
 struct OptionNone<T = String>(Option<T>);
 
@@ -214,6 +246,90 @@ impl<T> From<T> for OptionNone<T> {
     fn from(_field: T) -> Self {
         Self(None)
     }
+}
+
+#[derive(Serialize, Debug)]
+#[serde(transparent)]
+struct Unit<T = String> {
+    inner: (),
+    phantom: PhantomData<T>,
+}
+
+impl<T> Unit<T> {
+    fn new(_field: T) -> Self {
+        Self {
+            inner: (),
+            phantom: PhantomData,
+        }
+    }
+}
+
+macro_rules! impl_primitive_conversions {
+    ($(impl < T $(, $typevar:ident)? > $($trait:ident),+ for $($type:ident<T>),+;)+) => {
+        $(
+            impl_primitive_conversions! { @one_invocation; ($($type,)+); ($($trait,)+); ($($typevar)?); }
+        )+
+    };
+    (@one_invocation; ($($type:ident,)+); $traits:tt; $typevars:tt; ) => {
+        $(
+            impl_primitive_conversions! { @one_type; $type; $traits; $typevars; }
+        )+
+    };
+    (@one_type; $type:ident; ($($trait:ident,)+); $typevars:tt;) => {
+        $(
+            impl_primitive_conversions! {
+                @one_primitive;
+                $type;
+                $trait;
+                (
+                    i8, i16, i32, i64, i128,
+                    u8, u16, u32, u64, u128,
+                    f32, f64, char, bool,
+                    String, ByteBuf,
+                );
+                $typevars;
+            }
+        )+
+    };
+    (@one_primitive; $type:ident; $trait:ident; ($($primitive:ty,)+); $typevars:tt;) => {
+        $(
+            impl_primitive_conversions! {
+                @one_impl;
+                $type;
+                $trait;
+                $primitive;
+                $typevars;
+            }
+        )+
+    };
+    (@one_impl; $type:ident; AsRef; $primitive:ty; ($($typevar:ident)?);) => {
+        impl<T$(, $typevar)?> AsRef<$primitive> for $type<T>
+        where
+            T: AsRef<$primitive>,
+        {
+            fn as_ref(&self) -> &$primitive {
+                self.as_inner().as_ref()
+            }
+        }
+    };
+    (@one_impl; $type:ident; From; $primitive:ty; ($($typevar:ident)?);) => {
+        impl<T$(, $typevar)?> From<$primitive> for $type<T>
+        where
+            T: From<$primitive>,
+            $($typevar: Default,)?
+        {
+            fn from(field: $primitive) -> Self {
+                Self::new(T::from(field))
+            }
+        }
+    };
+    (@primitives) => {
+    };
+}
+
+impl_primitive_conversions! {
+    impl<T> From for Unit<T>;
+    impl<T> AsRef, From for NewType<T>;
 }
 
 macro_rules! tests {
@@ -227,100 +343,106 @@ macro_rules! tests {
     };
 }
 
-tests! {
-    fn test_string_to_ignored_any_ok<String, IgnoredAny>(check_ok_str_one_way);
-    fn test_string_and_newtype_ok<String, NewType>(check_equal_str_two_way);
-    fn test_string_and_tuple_struct_fail<String, TupleStruct>(check_fails_str_two_way);
-    fn test_string_and_field_struct_fail<String, FieldStruct>(check_fails_str_two_way);
-    fn test_string_and_newtype_variant_ok<String, NewTypeVariant>(check_equal_str_two_way);
-    fn test_string_and_tuple_variant_fail<String, TupleVariant>(check_fails_str_two_way);
-    fn test_string_and_struct_variant_fail<String, StructVariant>(check_fails_str_two_way);
-    fn test_string_and_option_some_ok<String, OptionSome>(check_equal_str_two_way);
-}
+//tests! {
+//    fn test_string_to_ignored_any_ok<String, IgnoredAny>(check_ok_str_one_way);
+//    fn test_string_and_newtype_equal<String, NewType>(check_equals_str_two_way);
+//    fn test_string_and_tuple_struct_fail<String, TupleStruct>(check_fails_str_two_way);
+//    fn test_string_and_field_struct_fail<String, FieldStruct>(check_fails_str_two_way);
+//    fn test_string_and_option_some_equal<String, Optional>(check_equals_str_two_way);
+//
+//    fn test_newtype_variant_to_string_fail<NewTypeVariant, String>(check_fails_str_one_way);
+//    fn test_tuple_variant_to_string_fail<TupleVariant, String>(check_fails_str_one_way);
+//    fn test_struct_variant_to_string_fail<StructVariant, String>(check_fails_str_one_way);
+//    fn test_option_none_to_string_fails<OptionNone, String>(check_fails_str_one_way);
+//    fn test_unit_to_string_fails<(), String>(check_fails_str_one_way);
+//    fn test_unit_struct_to_string_fails<UnitStruct, String>(check_fails_str_one_way);
+//    fn test_unit_variant_to_string_fails<UnitVariant, String>(check_fails_str_one_way);
+//
+//}
 
-pub(crate) fn check_equal_str_two_way<
-    T: DeserializeOwned + Serialize + Debug + AsRef<str> + From<String>,
-    U: DeserializeOwned + Serialize + Debug + AsRef<str> + From<String>,
->() {
-    check_equal_str_one_way::<T, U>();
-    check_equal_str_one_way::<U, T>();
-}
-
-pub(crate) fn check_equal_str_one_way<
-    T: Serialize + Debug + AsRef<str> + From<String>,
-    U: DeserializeOwned + Debug + AsRef<str>,
->() {
-    check_equal_str_one_way_leaf::<T, U>();
-    check_equal_str_one_way_leaf::<NewType<T>, NewType<U>>();
-    check_equal_str_one_way_leaf::<TupleStruct<T>, TupleStruct<U>>();
-    check_equal_str_one_way_leaf::<FieldStruct<T>, FieldStruct<U>>();
-    check_equal_str_one_way_leaf::<NewTypeVariant<T>, NewTypeVariant<U>>();
-    check_equal_str_one_way_leaf::<TupleVariant<T>, TupleVariant<U>>();
-    check_equal_str_one_way_leaf::<StructVariant<T>, StructVariant<U>>();
-    check_equal_str_one_way_leaf::<OptionSome<T>, OptionSome<U>>();
-}
-
-pub(crate) fn check_equal_str_one_way_leaf<
-    T: Serialize + Debug + AsRef<str> + From<String>,
-    U: DeserializeOwned + Debug + AsRef<str>,
->() {
-    check_evolution_ok::<_, U>(&T::from(String::new()), |old, new| {
-        old.as_ref() == new.as_ref()
-    });
-    check_evolution_ok::<_, U>(&T::from("string".to_owned()), |old, new| {
-        old.as_ref() == new.as_ref()
-    });
-}
-
-pub(crate) fn check_ok_str_one_way<
-    T: Serialize + Debug + From<String>,
-    U: DeserializeOwned + Debug,
->() {
-    check_ok_str_one_way_leaf::<T, U>();
-    check_ok_str_one_way_leaf::<NewType<T>, NewType<U>>();
-    check_ok_str_one_way_leaf::<TupleStruct<T>, TupleStruct<U>>();
-    check_ok_str_one_way_leaf::<FieldStruct<T>, FieldStruct<U>>();
-    check_ok_str_one_way_leaf::<NewTypeVariant<T>, NewTypeVariant<U>>();
-    check_ok_str_one_way_leaf::<TupleVariant<T>, TupleVariant<U>>();
-    check_ok_str_one_way_leaf::<StructVariant<T>, StructVariant<U>>();
-    check_ok_str_one_way_leaf::<OptionSome<T>, OptionSome<U>>();
-}
-
-pub(crate) fn check_ok_str_one_way_leaf<
-    T: Serialize + Debug + From<String>,
-    U: DeserializeOwned + Debug,
->() {
-    check_evolution_ok::<_, U>(&T::from(String::new()), |_, _| true);
-    check_evolution_ok::<_, U>(&T::from("string".to_owned()), |_, _| true);
-}
-
-pub(crate) fn check_fails_str_two_way<
-    T: DeserializeOwned + Serialize + Debug + From<String>,
-    U: DeserializeOwned + Serialize + Debug + From<String>,
->() {
-    check_fails_str_one_way::<T, U>();
-    check_fails_str_one_way::<U, T>();
-}
-
-pub(crate) fn check_fails_str_one_way<T, U>()
-where
-    T: Serialize + Debug + From<String>,
-    U: DeserializeOwned + Debug,
-{
-    check_fails_str_one_way_leaf::<T, U>();
-    check_fails_str_one_way_leaf::<NewType<T>, NewType<U>>();
-    check_fails_str_one_way_leaf::<TupleStruct<T>, TupleStruct<U>>();
-    check_fails_str_one_way_leaf::<FieldStruct<T>, FieldStruct<U>>();
-    check_fails_str_one_way_leaf::<NewTypeVariant<T>, NewTypeVariant<U>>();
-    check_fails_str_one_way_leaf::<TupleVariant<T>, TupleVariant<U>>();
-    check_fails_str_one_way_leaf::<StructVariant<T>, StructVariant<U>>();
-    check_fails_str_one_way_leaf::<OptionSome<T>, OptionSome<U>>();
-}
-
-pub(crate) fn check_fails_str_one_way_leaf<
-    T: Serialize + Debug + From<String>,
-    U: DeserializeOwned + Debug,
->() {
-    check_evolution_fails::<_, U>(&T::from(String::new()));
-    check_evolution_fails::<_, U>(&T::from("string".to_owned()));
-}
+//pub(crate) fn check_equals_str_two_way<
+//    T: DeserializeOwned + Serialize + Debug + AsRef<str> + From<String>,
+//    U: DeserializeOwned + Serialize + Debug + AsRef<str> + From<String>,
+//>() {
+//    check_equals_str_one_way::<T, U>();
+//    check_equals_str_one_way::<U, T>();
+//}
+//
+//pub(crate) fn check_equals_str_one_way<
+//    T: Serialize + Debug + AsRef<str> + From<String>,
+//    U: DeserializeOwned + Debug + AsRef<str>,
+//>() {
+//    check_equals_str_one_way_leaf::<T, U>();
+//    check_equals_str_one_way_leaf::<NewType<T>, NewType<U>>();
+//    check_equals_str_one_way_leaf::<TupleStruct<T>, TupleStruct<U>>();
+//    check_equals_str_one_way_leaf::<FieldStruct<T>, FieldStruct<U>>();
+//    check_equals_str_one_way_leaf::<NewTypeVariant<T>, Enum<U>>();
+//    check_equals_str_one_way_leaf::<TupleVariant<T>, Enum<U>>();
+//    check_equals_str_one_way_leaf::<StructVariant<T>, Enum<U>>();
+//    check_equals_str_one_way_leaf::<Optional<T>, Optional<U>>();
+//}
+//
+//pub(crate) fn check_equals_str_one_way_leaf<
+//    T: Serialize + Debug + AsRef<str> + From<String>,
+//    U: DeserializeOwned + Debug + AsRef<str>,
+//>() {
+//    check_evolution_ok::<_, U>(&T::from(String::new()), |old, new| {
+//        old.as_ref() == new.as_ref()
+//    });
+//    check_evolution_ok::<_, U>(&T::from("string".to_owned()), |old, new| {
+//        old.as_ref() == new.as_ref()
+//    });
+//}
+//
+//pub(crate) fn check_ok_str_one_way<
+//    T: Serialize + Debug + From<String>,
+//    U: DeserializeOwned + Debug,
+//>() {
+//    check_ok_str_one_way_leaf::<T, U>();
+//    check_ok_str_one_way_leaf::<NewType<T>, NewType<U>>();
+//    check_ok_str_one_way_leaf::<TupleStruct<T>, TupleStruct<U>>();
+//    check_ok_str_one_way_leaf::<FieldStruct<T>, FieldStruct<U>>();
+//    check_ok_str_one_way_leaf::<NewTypeVariant<T>, Enum<U>>();
+//    check_ok_str_one_way_leaf::<TupleVariant<T>, Enum<U>>();
+//    check_ok_str_one_way_leaf::<StructVariant<T>, Enum<U>>();
+//    check_ok_str_one_way_leaf::<Optional<T>, Optional<U>>();
+//}
+//
+//pub(crate) fn check_ok_str_one_way_leaf<
+//    T: Serialize + Debug + From<String>,
+//    U: DeserializeOwned + Debug,
+//>() {
+//    check_evolution_ok::<_, U>(&T::from(String::new()), |_, _| true);
+//    check_evolution_ok::<_, U>(&T::from("string".to_owned()), |_, _| true);
+//}
+//
+//pub(crate) fn check_fails_str_two_way<
+//    T: DeserializeOwned + Serialize + Debug + From<String>,
+//    U: DeserializeOwned + Serialize + Debug + From<String>,
+//>() {
+//    check_fails_str_one_way::<T, U>();
+//    check_fails_str_one_way::<U, T>();
+//}
+//
+//pub(crate) fn check_fails_str_one_way<T, U>()
+//where
+//    T: Serialize + Debug + From<String>,
+//    U: DeserializeOwned + Debug,
+//{
+//    check_fails_str_one_way_leaf::<T, U>();
+//    check_fails_str_one_way_leaf::<NewType<T>, NewType<U>>();
+//    check_fails_str_one_way_leaf::<TupleStruct<T>, TupleStruct<U>>();
+//    check_fails_str_one_way_leaf::<FieldStruct<T>, FieldStruct<U>>();
+//    check_fails_str_one_way_leaf::<NewTypeVariant<T>, Enum<U>>();
+//    check_fails_str_one_way_leaf::<TupleVariant<T>, Enum<U>>();
+//    check_fails_str_one_way_leaf::<StructVariant<T>, Enum<U>>();
+//    check_fails_str_one_way_leaf::<Optional<T>, Optional<U>>();
+//}
+//
+//pub(crate) fn check_fails_str_one_way_leaf<
+//    T: Serialize + Debug + From<String>,
+//    U: DeserializeOwned + Debug,
+//>() {
+//    check_evolution_fails::<_, U>(&T::from(String::new()));
+//    check_evolution_fails::<_, U>(&T::from("string".to_owned()));
+//}
